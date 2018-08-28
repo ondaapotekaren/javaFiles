c55f032059ba5c1c4383e3c006aa2bba4eba4189
ondaapotekaren <viktor_berglund@hotmail.com>
2018-08-28 10:50:11 +0200

    Day 10 finished, started day 11

--- Day10.java
+++ Day10.java
@@ -5,44 +5,64 @@ public class Day10 {
 
 	public static void main(String[] args) {
 		System.out.println("Day 10");
-		//System.out.println(Integer.parseInt(args[0]) % 5);
-		int[] lens = {63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24};
-		//int[] lens = {3,4,1,5};
+		String lens1 = "63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24";
+		int[] lens = new int[lens1.length()+5];
+		
+		// convert to ASCII
+		for(int i=0;i<lens1.length();i++){
+			lens[i] = (int)lens1.charAt(i);	
+		}
+
+		lens[lens1.length()] = 17;
+		lens[lens1.length()+1] = 31;
+		lens[lens1.length()+2] = 73;
+		lens[lens1.length()+3] = 47; 
+		lens[lens1.length()+4] = 23;
+
 		int[] knot = new int[256];
-		//int[] knot = new int[5];
 		int skipSize = 0;
 		int curr = 0;
-		//int start = 0;
-		int stop = 0;
 		for(int i = 0; i<knot.length;i++){
 			knot[i] = i;
 		}
-		for (int len : lens ) {
-			int start = curr;
-			stop = start + len-1;
-			while(start < stop) {
-				//System.out.println("start: "+start);
-				//System.out.println("stop: "+stop);
-				int tmp = knot[start % knot.length];
-				knot[start % knot.length] = knot[stop % knot.length];
-				knot[stop % knot.length] = tmp;
-				stop--;
-				start++;
+
+		for (int i = 0; i<64;i++) {
+			for (int len : lens ) {
+				int start = curr;
+				int stop = start + len-1;
+				while(start < stop) {
+					int tmp = knot[start % knot.length];
+					knot[start % knot.length] = knot[stop % knot.length];
+					knot[stop % knot.length] = tmp;
+					stop--;
+					start++;
+				}
+				curr = curr + len + skipSize; 
+				skipSize++;
 			}
-			curr = curr + len + skipSize; //% lens.length; //% lens.length;
-			skipSize++;
-			System.out.println("---------");
-			for (int i : knot) {
-				System.out.print(i + " ");
+			
+		}
+		// dense hash
+		int[] dens = new int[16];
+		int j = 0;
+		for(int i = 0;i<knot.length;i=i+16){
+			int xor = knot[i];
+			for(int ii = 1;ii<16;ii++){
+				xor = xor ^ knot[i+ii];
 			}
-			System.out.println();
+			dens[j] = xor;
+			j++;
 		}
-		System.out.println("-----");
-	 	for (int i : knot) {
-			System.out.print(i + " ");
+
+		// convert to hexa
+		String hex = "";
+		for(int i : dens){
+			String conv = Integer.toHexString(i);
+			if(conv.length() == 1){
+				conv = "0"+conv;
+			}
+			hex = hex + conv;
 		}
-		System.out.println("-----");
-		int a = knot[0]*knot[1];
-		System.out.println("Part A: "+a);
+		System.out.println("Part B: "+hex);
 	}
 }
---
+++ Day11.java
@@ -0,0 +1,7 @@
+public class Day11 {
+
+	public static void main(String[] args){
+		System.out.println("Day 11");
+	}
+
+}
