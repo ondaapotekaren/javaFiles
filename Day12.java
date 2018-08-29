0e68e675b1bbac2612e4d5d0b006af1cd576b0c3
ondaapotekaren <viktor_berglund@hotmail.com>
2018-08-29 12:41:27 +0200

    add correct solution to day 12

---
+++ Day12.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+public class Day12 {
+
+	public static List<Integer> createGroup(Integer input) {
+		List<Integer> group0 = new ArrayList<Integer>();
+		int index = 0;
+		group0.add(input);
+		while(index < group0.size()){
+			for(Integer i : graph.get(group0.get(index))) {
+				boolean inGroup = false;
+				for(Integer j : group0) {
+					if ((int)i == (int)j){
+						inGroup = true;
+						break;
+					}
+				} 		
+				if(!inGroup) {
+					group0.add(i);
+				}			 		
+			}
+			index++;
+		}
+		return group0;
+	}
+	
+	static List<List<Integer>> graph = new LinkedList<List<Integer>>();
+
+	public static void main(String[] args){
+		for(int i= 0;i<2000;i++) {
+			ArrayList<Integer> a = new ArrayList<Integer>();
+			graph.add(a);
+		}
+		try(BufferedReader br = new BufferedReader(new FileReader("./"+args[0]))) { 
+			String line;
+			while((line = br.readLine()) != null ) {
+				String[] splitList = line.split(" <-> " );
+				for (String s :  splitList[1].split(", ")) {
+					graph.get( (int)Integer.parseInt(splitList[0]) )
+						.add( (int)Integer.parseInt(s) );
+				}
+			} 
+		} catch (Exception e ) {
+			e.printStackTrace();
+		}
+		
+		List<List<Integer>> groupList = new LinkedList<List<Integer>>();
+		for(int i = 0;i<graph.size();i++) {
+			boolean inGroup = false;
+			for(List<Integer> l : groupList)
+				for(Integer j : l) {
+					if ((int) i == (int) j) {
+						inGroup = true;
+					}
+				}
+			if(!inGroup) {
+				groupList.add(createGroup(i));
+			}
+		}
+		System.out.println("Part A: "+groupList.get(0).size());
+		System.out.println("Part B: "+groupList.size());
+	}	
+}
