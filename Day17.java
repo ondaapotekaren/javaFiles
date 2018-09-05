c2bd46d57cfd7a473f5d5d5d7f3cf6127b0cb422
ondaapotekaren <viktor_berglund@hotmail.com>
2018-09-05 10:09:31 +0200

    simplified 17

--- Day17.java
+++ Day17.java
@@ -5,39 +5,26 @@ public class Day17 {
 
 	public static void main(String[] args) {
 	
-		LinkedList<Integer> ll = new LinkedList<Integer>();
+		LinkedList<Integer> buff = new LinkedList<Integer>();
 		int curr = 0;
-		ll.add(0);
+		buff.add(0);
 		int b = 0;
 		for(int i=1;i<50000001;i++){
 			curr = (curr + 345) % i;
-				
 			if(curr == 0) {
 				b = i;
 			}
-			if (curr == i-1) {
-				curr = i;
-			} else {
-				curr++;
-			}
-			
+			curr++;
 		}
 		
 		curr = 0;
 
 		for(int i = 1;i<2018;i++){
-			curr = (curr + 345) % ll.size();
-
-			if(curr == ll.size()-1){
-				ll.add(i);
-				curr = ll.size()-1;
-			} else {
-				ll.add(curr+1,i);
-				curr = curr +1;
-			}
-			
+			curr = (curr + 345) % i;
+			buff.add(curr+1,i);
+			curr++;
 		}
-		System.out.println("Part A: "+ll.get((curr+1) % ll.size()));
+		System.out.println("Part A: "+buff.get((curr+1) % buff.size()));
 		System.out.println("Part B: "+b);
 	}
 }
